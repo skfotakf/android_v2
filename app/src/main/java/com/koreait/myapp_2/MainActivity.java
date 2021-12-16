29ef2e1df2cd437fa7696b0a66e4e9c153b9b04d
DESKTOP-USQPRVG\gram <blueland1998@naver.com>
2021-12-16 17:44:11 +0900

    get id to java code

--- app/src/main/java/com/koreait/myapp_2/MainActivity.java
+++ app/src/main/java/com/koreait/myapp_2/MainActivity.java
@@ -3,6 +3,7 @@ package com.koreait.myapp_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvHello = findViewById(R.id.helloTv);
+        tvHello.setText("반갑습니다. 동적으로 글자 세팅 중");
     }
 }
