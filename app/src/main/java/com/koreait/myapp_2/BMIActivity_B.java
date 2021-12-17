bc5328e55c6f6d740106627402482bf21c5628e5
J1min <jmhong101506@gmail.com>
2021-12-17 17:54:28 +0900

    activity, intent

---
+++ app/src/main/java/com/koreait/myapp_2/BMIActivity_B.java
@@ -0,0 +1,14 @@
+package com.koreait.myapp_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BMIActivity_B extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bmiactivity_b);
+    }
+}
---
+++ app/src/main/java/com/koreait/myapp_2/IntentActivity_A.java
@@ -0,0 +1,14 @@
+package com.koreait.myapp_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntentActivity_A extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_activity);
+    }
+}
